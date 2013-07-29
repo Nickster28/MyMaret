754ca36c138456c5606fa7a67b97280ce126eaaf
Nick <user@server>
2013-07-29 00:30:13 -0400

    Setup sliding drawer UI for the main menu.

---
+++ MyMaret/AnnouncementsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AnnouncementsTableViewController.h
+//  MyMaret
+//
+//  Created by Nick Troccoli on 7/29/13.
+//  Copyright (c) 2013 Nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnnouncementsTableViewController : UITableViewController
+
+@end
