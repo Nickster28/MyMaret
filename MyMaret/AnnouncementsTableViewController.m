4a9fcb4b645b5f46b55df4043381faec2351ce49
Nick <user@server>
2013-10-15 20:51:13 -0700

    Made bottom toolbar not animated when it slides away.

--- MyMaret/AnnouncementsTableViewController.m
+++ MyMaret/AnnouncementsTableViewController.m
@@ -72,7 +72,7 @@
     // If there is no email address, the user is not an official Upper School user
     if (![[[NSUserDefaults standardUserDefaults] objectForKey:MyMaretUserEmailKey] isEqualToString:@""])
         [self.navigationController setToolbarHidden:NO animated:YES];
-    else [self.navigationController setToolbarHidden:YES animated:YES];
+    else [self.navigationController setToolbarHidden:YES animated:NO];
     
     if ([self shouldDisplayNewestAnnouncement]) {
         [self refreshAnnouncements];
